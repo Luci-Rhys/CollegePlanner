4909f8fff6683b9d454ed0e2b0abdc94fbb20faf
Luci-Rhys <155256360+Luci-Rhys@users.noreply.github.com>
2024-05-17 04:09:02 -0500

    iniial commit and import from different project that had issues running

---
+++ src/main/java/com/collegeplanner/CollegePlanner.java
@@ -0,0 +1,21 @@
+package com.collegeplanner;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class CollegePlanner extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Login login = new Login(primaryStage);
+        primaryStage.setScene(login.loginScene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+
+    }
+}
