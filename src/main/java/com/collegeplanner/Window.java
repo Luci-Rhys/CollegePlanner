c36fee789c0ee14a5fac4bb39b89caafaa0e433e
Luci-Rhys <155256360+Luci-Rhys@users.noreply.github.com>
2024-05-18 11:14:21 -0500

    added constructor to contain a stage and removed the stage parameter from dragWindow method

--- src/main/java/com/collegeplanner/Window.java
+++ src/main/java/com/collegeplanner/Window.java
@@ -13,14 +13,19 @@ public class Window {
     private HBox titleBar;
     private Stage primaryStage;
 
+    public Window(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
     //Enables movement of window by clicking and dragging the top pane of the window
-    public void dragWindow(HBox title, Stage primaryStage){
-        title.setOnMousePressed(press -> {
-            title.setOnMouseDragged(drag -> {
-                primaryStage.setX(drag.getScreenX() - press.getSceneX());
-                primaryStage.setY(drag.getScreenY() - press.getSceneY());
+    public void dragWindow(HBox title){
+
+            title.setOnMousePressed(press -> {
+                title.setOnMouseDragged(drag -> {
+                    primaryStage.setX(drag.getScreenX() - press.getSceneX());
+                    primaryStage.setY(drag.getScreenY() - press.getSceneY());
+                });
             });
-        });
     }
 
     //Method links a class's scene to its respective stylesheet(s)
