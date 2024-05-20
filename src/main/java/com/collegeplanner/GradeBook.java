c71cb74626616826fed0d5bd2d0979b2581d5020
Luci-Rhys <155256360+Luci-Rhys@users.noreply.github.com>
2024-05-19 19:08:19 -0500

    initial commit of inner class grade category, and addition of updategrade and calculate assignment grade

--- src/main/java/com/collegeplanner/GradeBook.java
+++ src/main/java/com/collegeplanner/GradeBook.java
@@ -1,4 +1,50 @@
 package com.collegeplanner;
 
+import java.util.ArrayList;
+
 public class GradeBook {
+    private ArrayList<Assignment> submittedAssignments;
+    private double courseGrade;
+
+    private void updateGrade(Assignment assignment, double pointsReceived) {
+        assignment.setPointsEarned(pointsReceived);
+    }
+
+    private double calcAssGrade(Assignment assignment){
+        double assignmentGrade = 0;
+
+        if(assignment.hasBeenSubmitted()){
+            assignmentGrade = (assignment.getPointsEarned())/(assignment.getPointsPossible());
+        }
+
+        return assignmentGrade;
+    }
+
+    class GradeCategory{
+
+        private String categoryName;
+        private double categoryWeight;
+
+        public GradeCategory(String categoryName, double categoryWeight){
+            this.categoryName = categoryName;
+            this.categoryWeight = categoryWeight;
+        }
+
+        public String getCategoryName() {
+            return categoryName;
+        }
+
+        public void setCategoryName(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        public double getCategoryWeight() {
+            return categoryWeight;
+        }
+
+        public void setCategoryWeight(double categoryWeight) {
+            this.categoryWeight = categoryWeight;
+        }
+
+    }
 }
